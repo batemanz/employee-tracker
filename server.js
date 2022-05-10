84ba63016767b08d82ff94d56ff5763dc795a231
Nathan Kilcrease <natekilcrease@gmail.com>
2022-05-10 12:02:42 -0700

    tables created in schema, installed packages, basic code setup

---
+++ server.js
@@ -0,0 +1,9 @@
+const inquirer = require('inquirer');
+const express = require('express');
+const mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'test'
+// });
